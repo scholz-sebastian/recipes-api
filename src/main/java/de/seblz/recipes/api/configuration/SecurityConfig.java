94c98d657208c843c49f8e6d7cf77639f07b6ee6
sebastian.scholz <scholz.sebastian@posteo.de>
2020-07-29 00:31:30 +0200

    fixed security config

--- src/main/java/de/seblz/recipes/api/configuration/SecurityConfig.java
+++ src/main/java/de/seblz/recipes/api/configuration/SecurityConfig.java
@@ -1,44 +1,38 @@
-//package de.seblz.recipes.api.configuration;
-//
-//import lombok.Data;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Data
-//@Configuration
-//@ConfigurationProperties(prefix = "recipe")
-//public class SecurityConfig implements WebMvcConfigurer {
-//
-//    private final Security security;
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping(security.apiMatcher)
-//            .allowedOrigins(security.getCors().getAllowedOrigins())
-//            .allowedMethods(security.getCors().getAllowedMethods())
-//            .allowedHeaders(security.getCors().getAllowedHeaders())
-//            .exposedHeaders(security.getCors().getExposedHeaders())
-//            .allowCredentials(security.getCors().getAllowCredentials())
-//            .maxAge(security.cors.getMaxAge());
-//    }
-//
-//    @Data
-//    public static class Security {
-//        private String apiMatcher;
-//        private Cors cors;
-//    }
-//
-//    @Data
-//    public static class Cors {
-//        private String[] allowedOrigins;
-//        private String[] allowedMethods;
-//        private String[] allowedHeaders;
-//        private String[] exposedHeaders;
-//        private Boolean allowCredentials;
-//        private Long maxAge;
-//    }
-//
-//}
+package de.seblz.recipes.api.configuration;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "security")
+public class SecurityConfig implements WebMvcConfigurer {
+
+    private Cors cors;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(cors.getAllowedOrigins())
+            .allowedMethods(cors.getAllowedMethods())
+            .allowedHeaders(cors.getAllowedHeaders())
+            .exposedHeaders(cors.getExposedHeaders())
+            .allowCredentials(cors.getAllowCredentials())
+            .maxAge(cors.getMaxAge());
+    }
+
+    @Data
+    public static class Cors {
+        private String[] allowedOrigins;
+        private String[] allowedMethods;
+        private String[] allowedHeaders;
+        private String[] exposedHeaders;
+        private Boolean allowCredentials;
+        private Long maxAge;
+    }
+
+}
